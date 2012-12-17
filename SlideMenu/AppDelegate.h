0300a9826a821b404b8e2ed02af7212b256ca6e4
richardcalahan <richard@calahan.me>
2012-12-17 09:18:00 -0800

    Initial Commit

---
+++ SlideMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlideMenu
+//
+//  Created by Richard Calahan on 12/17/12.
+//  Copyright (c) 2012 Richard Calahan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlideMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SlideMenu
+//
+//  Created by Richard Calahan on 12/17/12.
+//  Copyright (c) 2012 Richard Calahan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
